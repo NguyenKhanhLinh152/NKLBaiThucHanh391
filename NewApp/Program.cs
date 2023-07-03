810a4db9006f2d9e8d64f291a75e237f4b094c11
NguyenKhanhLinh152 <nklinh021502@gmail.com>
2023-07-03 17:14:42 +0700

    nhaptentuoi

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,4 +3,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("PTPMQL_Nhom_01");
 Console.WriteLine("Nhap ten cua ban: ");
 string name = Console.ReadLine();
-Console.WriteLine(name);
+Console.WriteLine("Nhap tuoi: ");
+string tuoi = Console.ReadLine();
+Console.WriteLine("Sinh vien: " + name + " Co tuoi la " + tuoi);
+
