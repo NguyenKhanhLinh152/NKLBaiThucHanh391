59bcf47675cc9be3319173f4c07cac7af8527728
NguyenKhanhLinh152 <97586831+NguyenKhanhLinh152@users.noreply.github.com>
2023-07-05 19:42:57 +0700

    Add files via upload

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,9 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("PTPMQL_Nhom_01");
-Console.WriteLine("Nhap ten cua ban: ");
-string name = Console.ReadLine();
-Console.WriteLine("Nhap tuoi: ");
-string tuoi = Console.ReadLine();
-Console.WriteLine("Sinh vien: " + name + " Co tuoi la " + tuoi);
-
+﻿//BTVN : Xay dung chuong trinh cho phép người dùng nhập thông tin sv , điểm môn học rồi tính điểm trung bình các môn học 
+ public class SinhVien
+{
+       public static void Main (string[] args )
+    {
+        System.Console.WriteLine("Nhap ten sinh vien:   ");
+        string Name = Console.ReadLine()!;
+        System.Console.WriteLine("Nhap ma so sinh vien:   ");
+        string ID = Console.ReadLine()!;
+            double a,b,c;
+            System.Console.Write("Điem Toan =  ");
+            a = Double.Parse(Console.ReadLine()!);
+            System.Console.Write("Điem Anh =  ");
+            b = Double.Parse(Console.ReadLine()!);
+            System.Console.Write("Diem Hóa =  ");
+            c = Double.Parse(Console.ReadLine()!);
+            double ĐTB = (a + b + c ) /3;
+            System.Console.WriteLine("Điem Trung Binh La = {0}",ĐTB);
+    }
+}
