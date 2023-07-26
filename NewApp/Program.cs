e21cd09be476e79705df8e97440a21966a902248
NguyenKhanhLinh152 <97586831+NguyenKhanhLinh152@users.noreply.github.com>
2023-07-26 13:41:33 +0700

    Add files via upload

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,20 +1,61 @@
-﻿//BTVN : Xay dung chuong trinh cho phép người dùng nhập thông tin sv , điểm môn học rồi tính điểm trung bình các môn học 
- public class SinhVien
+﻿using System.Collections;
+using System.Collections.Specialized;
+using NewApp.Models;
+public class Program
 {
-       public static void Main (string[] args )
+    public static void Main(string[] args)
     {
-        System.Console.WriteLine("Nhap ten sinh vien:   ");
-        string Name = Console.ReadLine()!;
-        System.Console.WriteLine("Nhap ma so sinh vien:   ");
-        string ID = Console.ReadLine()!;
-            double a,b,c;
-            System.Console.Write("Điem Toan =  ");
-            a = Double.Parse(Console.ReadLine()!);
-            System.Console.Write("Điem Anh =  ");
-            b = Double.Parse(Console.ReadLine()!);
-            System.Console.Write("Diem Hóa =  ");
-            c = Double.Parse(Console.ReadLine()!);
-            double ĐTB = (a + b + c ) /3;
-            System.Console.WriteLine("Điem Trung Binh La = {0}",ĐTB);
+        //Student std = new Student();
+        //std.EnterData();
+        //std.Display();
+        // Person ps = new Person();
+        // string str = "Nguyen khanh linh";
+        //int a = 21;
+        //System.Console.WriteLine("{0} sinh nam {1}",str,ps.GetYearOfBirth(a));
+        //* GPT PT = new GPT();
+        //PT.Phuongtrinhbacnhat();
+        //PT.GiaiPTB2(); 
+        //NHAP THONG TIN 
+        ArrayList arrStudent = new ArrayList();
+        System.Console.Write("nhap n=   ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for( int i = 0; i < n; i ++)
+        {
+            Student std = new Student ();
+            std.EnterData();
+            //ADD 
+            arrStudent.Add(std);
+        }
+        //HIEN THI THONG TIN TRONG ARRAY LIST
+        foreach(Student std in arrStudent)
+        {
+            std.Display();
+        }
+        
+        //SUA THONG TIN
+        System.Console.WriteLine("Nhap thong tin sinh vien muon sua: ");
+        string str = Console.ReadLine();
+        foreach (Student std in arrStudent)
+        {
+            if (std.StudentCode.Equals(std))
+            {
+                int intIndex = -1;
+                while(intIndex < 0 || intIndex >= n)
+                {
+                    System.Console.WriteLine("Nhap chi so cua phan tu muon sua: ");
+                    intIndex = Convert.ToInt32(Console.ReadLine());
+                    //khai bao 1 phan tu Student moi
+                    Student stdUpdate = new Student();
+                    stdUpdate.EnterData();
+                    //gan phan tu moi vao phan tu co index = intIndex
+                    arrStudent[intIndex] = stdUpdate;
+                }
+                foreach ( Student stdUpdate in arrStudent )
+                {
+                    stdUpdate.Display();
+                }
+            }
+            
+        }
     }
 }
